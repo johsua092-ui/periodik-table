60d5e8eb8626b3ecb7732439af0873624afd8634
johsua092-ui <johsua092@gmail.com>
2026-05-02 03:17:50 +0700

    Fix: Configure deployment for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Biar jadi file statis (HTML/CSS/JS)
+  basePath: '/periodik-table', // Sesuaikan sama nama repo lo
+  images: {
+    unoptimized: true, // GitHub Pages nggak dukung optimasi gambar bawaan Next.js
+  },
 };
 
 export default nextConfig;

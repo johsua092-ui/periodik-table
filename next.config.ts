d029d26dc4a5febff691166902764bfd6c5e9842
johsua092-ui <johsua092@gmail.com>
2026-05-02 03:41:56 +0700

    Fix: Add assetPrefix for GitHub Pages pathing

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/periodik-table',
+  assetPrefix: '/periodik-table/', // Nambahin prefix biar file JS/CSS nyari ke folder yang bener
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // Tambahan biar Build-nya nggak gampang error gara-gara hal sepele
   eslint: {
     ignoreDuringBuilds: true,
   },
